ada57bb435b75ac167edbf3292dae9357b9b43d6
Isaac Grimaldo <isaacgrimaldo432@gmail.com>
2022-05-12 16:11:05 -0500

    added confog to development

--- src/proyects/typescript/index.ts
+++ src/proyects/typescript/index.ts
@@ -1,23 +1,18 @@
+import {config} from 'dotenv';
+config();
+
 import { exec } from 'child_process';
-import simpleGit, { Options, TaskOptions } from 'simple-git';
+import simpleGit from 'simple-git';
 import fs from 'fs/promises';
 
 import utils from 'util';
 const execAsync = utils.promisify(exec);
 
 import { GlobalOptions } from '../../interfaces';
-import { gitRepos } from './repos/index';
+import { gitRepos,  options } from './repos/index';
+import { paths } from './helpers/path';
 
 const { repo  } = gitRepos;
-
-/**
- *
- */
-const options: TaskOptions<Options> = {
-    baseDir: process.cwd(),
-    binary: 'git',
-    maxConcurrentProcesses: 6,
-};
 const git = simpleGit(options);
 
 /**
@@ -26,16 +21,14 @@ const git = simpleGit(options);
 export const GenerateProjectTSC = async (prop: GlobalOptions) => {
     const { name, typeServer } = prop;
     const repoName = 'server-setups';
-    const cwd: string = process.cwd();
+    const {newPath , oldPath} = paths(repoName ,  name); 
 
     try {
         /**bring the files from the git repository specified*/
         const commands = ['clone' , '--branch' , typeServer , repo ]
         await git.raw(commands)
 
-        /**rename the folder for the name project*/
-        const oldPath = cwd + '/' + repoName;
-        const newPath = cwd + '/' + name;
+        /**rename the directory created for git clone*/
         await fs.rename(oldPath, newPath);
 
         /*deleted the git history of the new folder */
@@ -44,28 +37,28 @@ export const GenerateProjectTSC = async (prop: GlobalOptions) => {
         /**install dependencies from  npm*/
         console.log('\n' + '....installing dependencies wait a few moments');
         await execAsync('npm  install', {
-            cwd: cwd + '/' + name,
+            cwd: newPath,
         });
 
         console.clear();
 
         console.log();
         console.log('finished the installation process');
-        console.log('server type: ' + 'socker-server');
+        console.log('server type: ' + typeServer);
         console.log('');
         console.log('move at the new directory "cd ' + name + '"' + '\n');
     } catch (error) {
         console.log(error);
-        const checkDirs: string[] = await fs.readdir(cwd);
+        const checkDirs: string[] = await fs.readdir(newPath);
 
         //**directores controller if the something go wrong*/
 
         if (checkDirs.includes(name)) {
-            fs.rm(cwd + '/' + name, { recursive: true, force: true });
+            fs.rm(newPath, { recursive: true, force: true });
         }
 
         if (checkDirs.includes(repoName)) {
-            fs.rm(cwd + '/' + repoName, { recursive: true, force: true });
+            fs.rm(oldPath, { recursive: true, force: true });
         }
     }
 };
--- src/proyects/typescript/repos/index.ts
+++ src/proyects/typescript/repos/index.ts
@@ -1,3 +1,20 @@
+import {SimpleGitOptions} from "simple-git";
+import { config } from "dotenv";
+config();
+
+let path:string;
+if(process.env.NODE_ENV === "development" ){
+    path = process.cwd() + '/test'; 
+}else{
+    path = process.cwd();
+}
+
 export const gitRepos = {
     repo: 'https://github.com/isaacgrimaldo/server-setups.git',
+};
+
+export const options: Partial<SimpleGitOptions> = {
+    baseDir: path,
+    binary: 'git',
+    maxConcurrentProcesses: 6,
 };
